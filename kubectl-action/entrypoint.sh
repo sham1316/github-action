2e3d3c30426180dd2f70e4227cbc43645dcbc6b4
Alexey Pishchulin <sham1316@gmail.com>
2025-01-01 21:10:18 +0300

    kubectl-action: add

---
+++ kubectl-action/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -l
+
+mkdir ~/.kube/ || true
+if [ -f "~/.kube/config" ]; then
+  echo -e "\033[36mExisting kubeconfig found, using that and ignoring input\033[0m"
+else
+  echo -e "\033[36mUsing kubeconfig from input\033[0m"
+  echo "${INPUT_KUBECONFIG}" > ~/.kube/config
+fi
+
+echo -e "\033[36mPreparing execution\033[0m"
+echo "${INPUT_EXEC}" > run.sh
+chmod +x ./run.sh
+
+echo -e "\033[36mExecuting \033[0m"
+output=$(./run.sh)
+result=$?
+echo "$output"
+
+if [ $result -ne 0 ] ; then
+  echo "Exit code: $result"
+  exit 1
+fi
